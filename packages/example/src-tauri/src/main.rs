f47d386e83912886e44b58e89f173d7b7f656406
pujan shakya <pujan@rigonepal.com>
2024-08-20 10:17:16 +0545

    Checkpoint: tauri app added

--- packages/example/src-tauri/src/main.rs
+++ packages/example/src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
+use tauri_plugin_positioner::{Position, WindowExt};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,10 +12,33 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
 
     tauri::Builder::default()
         .plugin(tauri_plugin_positioner::init())
-        // .system_tray(SystemTray::new().with_menu(tray_menu))
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    ..
+                } => {
+                    let mut win = app.get_window("main").unwrap();
+                    let _ = win.move_window(Position::TrayCenter);
+                }
+                _ => {}
+            }
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import './App.css'
-import { Single } from './components/single'
+import './App.css';
+import { Single } from './components/single';
 
-import { move_window, Position } from "tauri-plugin-positioner-api";
 function App() {
 
-  const x = React.useCallback(() => {
-    move_window(Position.RightCenter);
-  }, [])
-
-
-  React.useEffect(() => { x?.() }, [x])
- 
   return (
     <Single />
   )
